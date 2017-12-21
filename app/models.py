5a47c749342082f951c631464ee66e7fe5b62a68
pointatnick <nicholashma@gmail.com>
2017-12-20 22:29:57 -0800

    remove models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
 
+
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140), index=True, unique=True)
 
     def __repr__(self):
-        return "<Character %r>" % (self.name)
+        return "<Character %r>" % (self.name) 
