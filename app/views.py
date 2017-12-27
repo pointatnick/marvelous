3647e9bd2a1e8c05395991b9264d51f1f7011754
pointatnick <nicholashma@gmail.com>
2017-12-27 14:08:01 -0800

    merge

--- app/views.py
+++ app/views.py
@@ -12,11 +12,8 @@ def index():
     public_key = '8bfbc491d6c583ad498640d6fa843487'
     private_key = '0a9946e6e4f1cedbe03d7074f5feca19b33b3757'
     ts = time.time()
-    print(ts)
     hash_key = str(ts) + private_key + public_key
-    print(hash_key)
     m = hashlib.md5(hash_key.encode('utf-8')).hexdigest()
-    print(m)
     r = requests.get(
         'https://gateway.marvel.com:443/v1/public/characters?ts=' + str(ts)
         + '&apikey=' + public_key
@@ -41,5 +38,3 @@ def test():
         #     pass
     elif request.method == 'GET':
         return render_template('test.html')
-
->>>>>>> Stashed changes
