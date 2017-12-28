b4c62143e274f7b0662242fbd2dbf9a865aca498
pointatnick <nicholashma@gmail.com>
2017-12-27 18:19:05 -0800

    add comments and comic list functions

--- app/utils.py
+++ app/utils.py
@@ -3,8 +3,12 @@ import hashlib
 import requests
 
 
+public_key = '8bfbc491d6c583ad498640d6fa843487'
+private_key = '0a9946e6e4f1cedbe03d7074f5feca19b33b3757'
+
+
 def parse_input(input):
-    # given a string, returns a list of strings, delimited by ','
+    # given a string, returns a list of strings
     # entries in result list do not have leading/ending whitespace
     characters = input.split(',')
     formatted_characters = []
@@ -17,8 +21,6 @@ def parse_input(input):
 
 def get_cid(character):
     # given a character, returns the ID of that character from marvel API
-    public_key = '8bfbc491d6c583ad498640d6fa843487'
-    private_key = '0a9946e6e4f1cedbe03d7074f5feca19b33b3757'
     ts = time.time()
     hash_key = str(ts) + private_key + public_key
     m = hashlib.md5(hash_key.encode('utf-8')).hexdigest()
@@ -26,17 +28,53 @@ def get_cid(character):
         'https://gateway.marvel.com:443/v1/public/characters?ts={}&apikey={}&hash={}&name={}'.
         format(str(ts), public_key, m, character))
     result = r.json()['data']['results']
+    # if API lookup returns no result, we return None
     if result == []:
         return None
     else:
         return result[0]['id']
 
 
+def get_comic_list(cid):
+    # given a character ID, returns the list of comics that character is featured in
+    # marvel API only allows queries of 100 things at a time
+    MARVEL_LIMIT = 100
+
+    ts = time.time()
+    hash_key = str(ts) + private_key + public_key
+    m = hashlib.md5(hash_key.encode('utf-8')).hexdigest()
+    offset = 0  # need to keep track of this to request next 100 titles
+    titles = []
+    while True:
+        r2 = requests.get(
+            'https://gateway.marvel.com:443/v1/public/characters/{}/comics?ts={}&apikey={}&hash={}&limit={}&offset={}'
+            .format(
+                str(cid), str(ts), public_key, m, str(MARVEL_LIMIT),
+                str(offset)))
+        count = r2.json()['data']['count']
+        results = r2.json()['data']['results']
+        offset = offset + MARVEL_LIMIT
+        for result in results:
+            titles.append(result['title'])
+        if count < MARVEL_LIMIT:
+            break
+    print(titles)
+    return titles
+
+
+def build_comic_lists(cids):
+    # given a list of character IDs, assembles comic lists from those IDs
+    comic_lists = []
+    for cid in cids:
+        comic_lists.append(get_comic_list(cid))
+    return comic_lists
+
+
 def find_common_list(comics_lists):
     # given a list of a list of comic titles,
     # returns list of titles in both sets
-    comics_sets = []
-    for comics in comics_lists:
-        comics_sets.append(set(comics))
+    comic_sets = []
+    for comic_list in comics_lists:
+        comic_sets.append(set(comic_list))
 
-    return set.intersection(*comics_sets)
+    return set.intersection(*comic_sets)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,5 @@
 from app import app, utils
 from flask import request, render_template, url_for
-import requests
-import hashlib
-import time
 
 
 @app.route('/')
@@ -18,9 +15,6 @@ def test():
 
 @app.route('/results')
 def results():
-    # marvel API only allows queries of 100 things at a time
-    MARVEL_LIMIT = 100
-
     # get list of characters from input
     characters = utils.parse_input(request.args.get('text', ''))
 
@@ -29,24 +23,6 @@ def results():
     for character in characters:
         cids.append(utils.get_cid(character))
 
-    # ts = time.time()
-    # hash_key = str(ts) + private_key + public_key
-    # m = hashlib.md5(hash_key.encode('utf-8')).hexdigest()
-    # offset = 0
-    # titles = []
-    # while True:
-    #     r2 = requests.get(
-    #         'https://gateway.marvel.com:443/v1/public/characters/{}/comics?ts={}&apikey={}&hash={}&limit={}&offset={}'
-    #         .format(
-    #             str(cid), str(ts), public_key, m, str(MARVEL_LIMIT),
-    #             str(offset)))
-    #     count = r2.json()['data']['count']
-    #     results = r2.json()['data']['results']
-    #     offset = offset + MARVEL_LIMIT
-    #     for result in results:
-    #         titles.append(result['title'])
-    #     print(titles, len(titles))
-    #     if count < MARVEL_LIMIT:
-    #         break
-    # comics = utils.find_common_list(#list of comics)
+    comic_lists = utils.build_comic_lists(cids)
+    comics = utils.find_common_list(comic_lists)
     return render_template('results.html', comics=comics)
