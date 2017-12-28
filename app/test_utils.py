c83d463cf8566b4e32b53fd9c08cc0a7c0e8fa50
Danielwang405 <danielwang405@gmail.com>
2017-12-27 17:59:00 -0800

    added test case for utils

--- app/test_utils.py
+++ app/test_utils.py
@@ -1,7 +1,13 @@
 from app import utils
 
 
-def test_parse_input():
-    assert utils.parse_input('') == []
-    assert utils.parse_input('Spider-Man') == ['Spider-Man']
+# def test_parse_input():
+#     assert utils.parse_input('') == []
+#     assert utils.parse_input('Spider-Man') == ['Spider-Man']
+
+def test_find_common_list():
+    assert utils.find_common_list([[1,2,3],[2,3,4]]) == {2,3}
+    assert utils.find_common_list([[],[]]) == set()
+    assert utils.find_common_list([[],[],[]]) == set()
+    assert utils.find_common_list([[1],[2]]) == set()
     
